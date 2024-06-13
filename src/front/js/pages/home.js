317621dccaf288423a9568e5a4f4897a980f3024
Aresdgi <115067719+Aresdgi@users.noreply.github.com>
2024-06-13 09:40:03 +0000

    Refactor Home component: removed image and added LoginForm and ErrorAlert components

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,19 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import LoginForm from "../component/login_form.jsx";
+import ErrorAlert from "../component/error.jsx";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+	const { store } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+		<div className="container mx-auto mt-3">
+			<div className="row">
+				{store.login_error && <ErrorAlert error={store.login_error} />}
+				<LoginForm />
+
 			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
 		</div>
 	);
 };
