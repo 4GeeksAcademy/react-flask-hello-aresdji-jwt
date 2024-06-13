15e17c17a2931d85d3497e6e73d2518599f947e4
Aresdgi <115067719+Aresdgi@users.noreply.github.com>
2024-06-13 09:44:45 +0000

    Refactor getState: replaced demo state with authentication and user management logic

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,51 +1,72 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			access_token: "",
+			login_error: "",
+			signup_error: "",
+			valid_token: false,
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
+			login: (email, password) => {
+				const credentials = { "email": email, "password": password}
+				const config = { 
+					method: "POST",
+					body: JSON.stringify(credentials),
+					headers: { "Content-Type": "application/json" }
 				}
+
+				fetch(process.env.BACKEND_URL + "/login", config)
+				.then((response) => {
+					if (!response.ok) {
+						return response.json().then((error) => {
+							setStore({ login_error: error.Error })
+						  	throw new Error(error.Error);
+						});
+					} else return response.json(); 
+				})
+				.then((data) => setStore({ access_token: data.access_token, login_error: "", signup_error: "" }))
+				.catch((error) => console.log(error))
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+			logout: () => {
+				setStore({ access_token: "", login_error: "", signup_error: "" })
+			},
 
-				//reset the global store
-				setStore({ demo: demo });
+			createUser: (username, email, password) => {
+				const newUser = { "username": username, "email": email, "password": password }
+				const config = { 
+					method: "POST",
+					body: JSON.stringify(newUser),
+					headers: { "Content-Type": "application/json" }
+				}
+
+				fetch(process.env.BACKEND_URL + "/users", config)
+				.then((response) => {
+					if (!response.ok) {
+						return response.json().then((error) => {
+							setStore({ signup_error: error.Error })
+						  	throw new Error(error.Error);
+						});
+					} else getActions().login(email, password)
+				})
+				.catch((error) => console.log(error))
+			},
+
+			validateToken: async () => {
+				try {
+					const response = await fetch(process.env.BACKEND_URL + "/validate-token", {
+						method: 'GET',
+						headers: {
+							'Authorization': `Bearer ${getStore().access_token}`
+						}
+					});
+					const data = await response.json();
+					setStore({ valid_token: data.valid });
+					return data.valid; 
+				} catch (error) {
+					console.error(error);
+					return false; 
+				}
 			}
 		}
 	};
